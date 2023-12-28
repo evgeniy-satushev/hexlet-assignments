04c6ca1514c4709c46c163356149798fa0cb9393
@hexlet/cli <support@hexlet.io>
2023-12-28 16:51:35 +0300

    submit java-collections-ru/other-collections

--- java-collections-ru/other-collections/src/main/java/exercise/App.java
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -1,28 +1,25 @@
 package exercise;
 
-
 import java.util.*;
+import java.util.stream.Collectors;
 
 // BEGIN
 public class App {
     public static Map<String, Object> genDiff(Map<String, Object> firstMap, Map<String, Object> secondMap) {
-        Map<String, Object> test = new LinkedHashMap<>();
-        Map<String, Object> combainedMap = new HashMap<>(firstMap);
-        combainedMap.putAll(secondMap);
-        for (Map.Entry<String, Object> entry: combainedMap.entrySet()) {
-            boolean isEqualKeys = secondMap.containsKey(entry.getKey()) && firstMap.containsKey(entry.getKey());
-            boolean isEqualValues = secondMap.containsValue(entry.getValue()) && firstMap.containsValue(entry.getValue());
-            if (!firstMap.containsKey(entry.getKey())) {
-                test.put(entry.getKey(), "added");
-            } else if (!secondMap.containsKey(entry.getKey())) {
-                test.put(entry.getKey(), "deleted");
-            } else if (isEqualKeys && !isEqualValues) {
-                test.put(entry.getKey(), "changed");
+        Set<String> temp = new HashSet<>(firstMap.keySet());
+        temp.addAll(secondMap.keySet());
+        return temp.stream()
+                .collect(Collectors.toMap(key -> key, value -> {
+            if (!firstMap.containsKey(value)) {
+                return "added";
+            } else if (!secondMap.containsKey(value)) {
+                return "deleted";
+            } else if (secondMap.get(value).equals(firstMap.get(value))) {
+                return "unchanged";
             } else {
-                test.put(entry.getKey(), "unchanged");
+                return "changed";
             }
-        }
-        return test;
+        }));
     }
 }
 //END
